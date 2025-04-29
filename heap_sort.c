722095ba557e7f4d61be1b27a392c0f220c95731
shinchana1011 <shinchanar.ai23@bmsce.ac.in>
2025-04-29 11:06:25 +0530

    Update heap_sort.c

--- heap_sort.c
+++ heap_sort.c
@@ -1,52 +1,51 @@
+
+
 #include <stdio.h>
-
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest != i) {
-        int temp = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = temp;
-
-        heapify(arr, n, largest);
+void heapify(int arr[], int n, int i)
+{
+    int temp, maximum, left_index, right_index;
+    maximum = i;
+    right_index = 2 * i + 2;
+    left_index = 2 * i + 1;
+    if (left_index < n && arr[left_index] > arr[maximum])
+        maximum = left_index;
+    if (right_index < n && arr[right_index] > arr[maximum])
+        maximum = right_index;
+    if (maximum != i) {
+        temp = arr[i];
+        arr[i] = arr[maximum];
+        arr[maximum] = temp;
+        heapify(arr, n, maximum);
     }
 }
-
-void heapSort(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--)
+void heapsort(int arr[], int n)
+{
+    int i, temp;
+    for (i = n / 2 - 1; i >= 0; i--) {
         heapify(arr, n, i);
-
-    for (int i = n - 1; i >= 0; i--) {
-        int temp = arr[0];
+    }
+    for (i = n - 1; i > 0; i--) {
+        temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
-
         heapify(arr, i, 0);
     }
 }
+int main()
+{
+    int arr[] = { 20, 18, 5, 15, 3, 2 };
+    int n = 6;
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; ++i)
+    int i;
+    printf("Original Array : ");
+    for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
+    }
     printf("\n");
-}
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    heapSort(arr, n);
-
-    printf("Sorted array is\n");
-    printArray(arr, n);
-
+    heapsort(arr, n);
+    printf("Array after performing heap sort: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
